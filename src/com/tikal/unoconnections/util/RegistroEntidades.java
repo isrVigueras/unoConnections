a0da5faf60af771f6389add67754bf5f70930e66
Israel <Israel@LAPTOP-KDKMA59E>
2017-11-10 11:24:48 -0600

    inicio

--- src/com/tikal/unoconnections/util/RegistroEntidades.java
+++ src/com/tikal/unoconnections/util/RegistroEntidades.java
@@ -1,12 +1,8 @@
 package com.tikal.unoconnections.util;
 
-import com.googlecode.objectify.ObjectifyService;
-import com.tikal.unoconnections.model.Item;
-
 public class RegistroEntidades {
 	
 	public RegistroEntidades(){
-		ObjectifyService.register(Item.class);
 	}
 
 }

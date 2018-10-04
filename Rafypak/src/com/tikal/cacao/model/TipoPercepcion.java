b40a8e472b0f4c95327db9d167bd65fba2e1cc53
Israel <Israel@LAPTOP-KDKMA59E>
2018-10-04 12:57:28 -0500

    part 3

--- Rafypak/src/com/tikal/cacao/model/TipoPercepcion.java
+++ Rafypak/src/com/tikal/cacao/model/TipoPercepcion.java
@@ -13,10 +13,10 @@ public enum TipoPercepcion {
 	@SerializedName("Sueldos, Salarios Rayas y Jornales")
 	SUELDOS_SALARIOS_RAYAS_JORNALES("Sueldos Salarios Rayas Jornales", true),
 	
-	@SerializedName("Gratificación Anual")
+	@SerializedName("Gratificaciï¿½n Anual")
 	GRATIFICACION_ANUAL_AGUINALDO("Gratificacion Anual Aguinaldo", true),
 	
-	@SerializedName("Participación de los trabajadores en las Utilidades PTU")
+	@SerializedName("Participaciï¿½n de los trabajadores en las Utilidades PTU")
 	PARTICIPACION_DE_LOS_TRABAJADORES_EN_LAS_UTILIDADES_PTU("Participacion de los Trabajadores en las Utilidades PTU", true), 
 	
 	@SerializedName("Fondo de Ahorro")
@@ -28,7 +28,7 @@ public enum TipoPercepcion {
 	@SerializedName("Caja de Ahorro")
 	CAJA_DE_AHORRO("Caja de Ahorro", false),
 	
-	@SerializedName("Contribuciones a Cargo del Trabajador Pagadas por el Patrón")
+	@SerializedName("Contribuciones a Cargo del Trabajador Pagadas por el Patrï¿½n")
 	CONTRIBUCIONES_A_CARGO_DEL_TRABAJADOR_PAGADAS_POR_EL_PATRON("Contribuciones a Cargo del Trabajador Pagadas por el Patron", false),
 	
 	@SerializedName("Premios por puntualidad")
@@ -37,10 +37,10 @@ public enum TipoPercepcion {
 	@SerializedName("Prima de Seguro de vida")
 	PRIMA_DE_SEGURO_DE_VIDA("Prima de Seguro de vida", true),
 	
-	@SerializedName("Seguro de Gastos Médicos Mayores")
+	@SerializedName("Seguro de Gastos Mï¿½dicos Mayores")
 	SEGURO_DE_GASTOS_MEDICOS_MAYORES("Seguro de Gastos Medicos Mayores", false),
 	
-	@SerializedName("Cuotas Sindicales Pagadas por el Patrón")
+	@SerializedName("Cuotas Sindicales Pagadas por el Patrï¿½n")
 	CUOTAS_SINDICALES_PAGADAS_POR_EL_PATRON("Cuotas Sindicales Pagadas por el Patron", false),
 	
 	@SerializedName("Subsidios por incapacidad")
@@ -64,10 +64,10 @@ public enum TipoPercepcion {
 	@SerializedName("Prima Vacacional")
 	PRIMA_VACACIONAL("Prima vacacional", true),
 	
-	@SerializedName("Prima por antigüedad")
+	@SerializedName("Prima por antigï¿½edad")
 	PRIMA_POR_ANTIGUEDAD("Prima por antiguedad", true),
 	
-	@SerializedName("Pagos por Separación")
+	@SerializedName("Pagos por Separaciï¿½n")
 	PAGOS_POR_SEPARACION("Pagos por separacion", true),
 	
 	@SerializedName("Seguro de Retiro")
@@ -79,7 +79,7 @@ public enum TipoPercepcion {
 	@SerializedName("Reembolso por funeral")
 	REEMBOLSO_POR_FUNERAL("Reembolso por funeral", false),
 	
-	@SerializedName("Cuotas de seguridad social pagadas por el patrón")
+	@SerializedName("Cuotas de seguridad social pagadas por el patrï¿½n")
 	CUOTAS_DE_SEGURIDAD_SOCIAL_PAGADAS_POR_EL_PATRON("Cuotas de seguridad social pagadas por el patron", false),
 	
 	@SerializedName("Comisiones")
@@ -92,7 +92,7 @@ public enum TipoPercepcion {
 	VALES_DE_RESTAURANTE("Vales de restaurante", true),
 	
 	@SerializedName("Vales de gasolina")
-	VALES_DE_GASOLINA("Vales_de_gasolina", false), // esta exento sólo si están a nombre de la dependencia
+	VALES_DE_GASOLINA("Vales_de_gasolina", false), // esta exento sï¿½lo si estï¿½n a nombre de la dependencia
 	
 	@SerializedName("Vales de Ropa")
 	VALES_DE_ROPA("Vales_de_ropa", true),
@@ -100,14 +100,14 @@ public enum TipoPercepcion {
 	@SerializedName("Ayuda para renta")
 	AYUDA_PARA_RENTA("Ayuda para renta", true),
 	
-	@SerializedName("Ayuda para artículos escolares")
+	@SerializedName("Ayuda para artï¿½culos escolares")
 	AYUDA_PARA_ARTICULOS_ESCOLARES("Ayuda para articulos escolares", true),
 	
 	@SerializedName("Ayuda para anteojos")
 	AYUDA_PARA_ANTEOJOS("Ayuda para anteojos", true),
 	
 	@SerializedName("Ayuda para transporte")
-	AYUDA_PARA_TRANSPORTE("Ayuda para transporte", true), // si es vehículo de la empresa se exenta
+	AYUDA_PARA_TRANSPORTE("Ayuda para transporte", true), // si es vehï¿½culo de la empresa se exenta
 	
 	@SerializedName("Ayuda para gastos de funeral")
 	AYUDA_PARA_GASTOS_DE_FUNERAL("Ayuda para gastos de funeral", false),
@@ -121,26 +121,26 @@ public enum TipoPercepcion {
 	@SerializedName("Jubilaciones, pensiones o haberes de retiro en parcialidades")
 	JUBILACIONES_PENSIONES_O_HABERES_DE_RETIRO_PARCIALIDADES("Jubilaciones, pensiones o haberes de retiro en parcialidades", true),
 	
-	@SerializedName("Ingresos en acciones o títulos valor que representan bienes")
-	INGRESOS_ACCIONES_TITULOS_VALOR_QUE_REPRESENTAN_BIENES("Ingresos en acciones o títulos valor que representan bienes", false),
+	@SerializedName("Ingresos en acciones o tï¿½tulos valor que representan bienes")
+	INGRESOS_ACCIONES_TITULOS_VALOR_QUE_REPRESENTAN_BIENES("Ingresos en acciones o tï¿½tulos valor que representan bienes", false),
 	
 	@SerializedName("Ingresos asimilados a salarios")
 	INGRESOS_ASIMILADOS_A_SALARIOS("Ingresos asimilados a salarios", false),
 	
-	@SerializedName("Alimentación")
-	ALIMENTACION("Alimentación", false),
+	@SerializedName("Alimentaciï¿½n")
+	ALIMENTACION("Alimentaciï¿½n", false),
 	
-	@SerializedName("Habitación")
-	HABITACION("Habitación", false),
+	@SerializedName("Habitaciï¿½n")
+	HABITACION("Habitaciï¿½n", false),
 	
 	@SerializedName("Premios por asistencia")
 	PREMIOS_POR_ASISTENCIA("Premios por asistencia", true),
 	
-	// en la versión para CDFI 3.3 para 2017 ya no se incluyen los tipos de percepciones siguientes
-	// para los valores siguientes aún no se determina si gravan o no para ISR
+	// en la versiï¿½n para CDFI 3.3 para 2017 ya no se incluyen los tipos de percepciones siguientes
+	// para los valores siguientes aï¿½n no se determina si gravan o no para ISR
 	INGRESO_PAGADO_POR_ENTIDADES_FEDERATIVAS_MUNICIPIOS_O_DEMARCACIONES_TERRITORIALES_DEL_DISTRITO_FEDERAL_ORGANISMOS_AUTONOMOS_Y_ENTIDADES_PARAESTATALES_Y_PARAMUNICIPALES_CON_INGRESOS_PROPIOS(""
 			+ "Ingreso pagado por Entidades federativas municipios o demarcaciones territoriales del Distrito Federal organismos autonomos y entidades paraestatales y paramunicipales con ingresos propios", false),
-	INGRESO_PAGADO_POR_ENTIDADES_FEDERATIVAS_MUNICIPIOS_O_DEMARCACIONES_TERRITORIALES_DEL_DISTRITO_FEDERAL_ORGANISMOS_AUTONOMOS_Y_ENTIDADES_PARAESTATALES_Y_PARAMUNICIPALES_CON_INGRESOS_FEDERAÑES(""
+	INGRESO_PAGADO_POR_ENTIDADES_FEDERATIVAS_MUNICIPIOS_O_DEMARCACIONES_TERRITORIALES_DEL_DISTRITO_FEDERAL_ORGANISMOS_AUTONOMOS_Y_ENTIDADES_PARAESTATALES_Y_PARAMUNICIPALES_CON_INGRESOS_FEDERALES(""
 			+ "Ingreso pagado por Entidades federativas municipios o demarcaciones territoriales del Distrito Federal organismos autonomos y entidades paraestatales y paramunicipales con ingresos federales", false),
 	INGRESO_PAGADO_POR_ENTIDADES_FEDERATIVAS_MUNICIPIOS_O_DEMARCACIONES_TERRITORIALES_DEL_DISTRITO_FEDERAL_ORGANISMOS_AUTONOMOS_Y_ENTIDADES_PARAESTATALES_Y_PARAMUNICIPALES_CON_INGRESOS_PROPIOS_Y_FEDERALES(""
 			+ "Ingreso pagado por Entidades federativas municipios o demarcaciones territoriales del Distrito Federal organismos autonomos y entidades paraestatales y paramunicipales con ingresos propios y federales", false);
